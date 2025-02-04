dafaf5f543704aa138197ff19c230627371152d4
Koos <kkposbus@gmail.com>
2025-02-04 19:06:55 +0200

    Updated data that is shown in animal list

--- src/main/java/koossa/plaasbestuur/fxml/livestock/util/ListEntryElement.java
+++ src/main/java/koossa/plaasbestuur/fxml/livestock/util/ListEntryElement.java
@@ -1,22 +1,25 @@
 package koossa.plaasbestuur.fxml.livestock.util;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import koossa.plaasbestuur.data.livestock.LivestockEntry;
 
-public class ListEntryElement extends VBox {
+public class ListEntryElement extends HBox {
 	
 	private LivestockEntry entry;
-	private Label name_lbl, age_lbl, race_lbl;
+	private Label name_lbl, age_lbl, race_lbl, location_lbl;
 	
 	public ListEntryElement(LivestockEntry entry) {
 		this.entry = entry;
 		this.name_lbl = new Label(entry.getBrand());
-		this.age_lbl = new Label(Integer.toString(entry.getAgeYears()));
-		this.race_lbl = new Label(entry.getRace());
+		this.age_lbl = new Label("Age: " + Integer.toString(entry.getAgeYears()));
+		this.race_lbl = new Label(entry.getRace() + " - " + entry.getGender());
+		this.location_lbl = new Label("@ " + entry.getLocation());
 		
-		this.getChildren().addAll(name_lbl, race_lbl, age_lbl);
+		this.getChildren().addAll(name_lbl, race_lbl, age_lbl, location_lbl);
 		this.setSpacing(10);
+		this.setAlignment(Pos.TOP_CENTER);
 	}
 	
 	public LivestockEntry getEntry() {
